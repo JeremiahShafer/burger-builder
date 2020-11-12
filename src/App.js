e6d71354a5cf59419441d19012ebc9c368625870
JeremiahShafer <jrobertshafer@icloud.com>
2020-11-11 22:32:38 -0600

    Updated components and created higher-order component as JSX wrapper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+			  <Layout>
+					<p>Is this thing on?</p>
+				</Layout>
       </div>
     );
   }
