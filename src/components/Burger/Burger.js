28ab067d6542019a6e436a5c61b339e163f884e5
JeremiahShafer <jrobertshafer@icloud.com>
2020-11-12 18:48:10 -0600

    Enabled dynamic ingredient count

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,12 +5,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = ( props ) => {
-	const transformedIngredients = Object.keys(props.ingredients)
+	let transformedIngredients = Object.keys(props.ingredients)
 		.map(igKey => {
 			return [...Array(props.ingredients[igKey])].map((_, i) => {
 				return <BurgerIngredient key={igKey + i} type={igKey} />
 			}); //Array with two elements [-,-]
-		});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []); // Flatten the arrray
+	if (transformedIngredients.length === 0 ) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+	}
   return(
 		<div className={classes.Burger}>
 			<BurgerIngredient type="bread-top" />
